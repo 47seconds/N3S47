498a9bcdb81257ed049863cdc3703d2211b1b80a
47seconds <vaibhav@vocallabs.ai>
2026-03-26 03:01:11 +0530

    AND instruction

--- src/cpu.cpp
+++ src/cpu.cpp
@@ -17,17 +17,17 @@ uint8_t CPU::ADC() {
   */
 
   // Sum = A + operand + carry (read Carry bit from P)
-  uint16_t sum = (uint16_t)A + (uint16_t)fetched_val + (P & C);
+  uint16_t sum = (uint16_t)A + (uint16_t)fetched_val + (P & C ? 1 : 0);
 
-  // if overflow, set carry bit, else set it to 0
+  // if res > uint8_t size, set carry bit, else set it to 0
   if (sum > 0xFF) P |= C; // Set carry bit in P (since C is already 1 in struct)
-  else P &= (~C);
+  else P &= ~C;
 
   uint8_t res = sum & 0xFF; // can use (uint8_t)sum, but conversion -> compiler decides, while rn we mark explicitly
 
   // set zero bit, if applicable
   if (!res) P |= Z;
-  else P &= !Z;
+  else P &= ~Z;
 
   // set negative if last bit is set (Xxxxxxxx & 10000000 (0x80))
   if (res & 0x80) P |= N;
@@ -40,4 +40,18 @@ uint8_t CPU::ADC() {
   A = res;
 
   return 1; // need 1 extra if page crossed (https://www.nesdev.org/obelisk-6502-guide/reference.html)
+}
+
+uint8_t CPU::AND() {
+  uint8_t res = A & fetched_val;
+
+  if (!res) P |= Z;
+  else P &= ~Z;
+
+  if (res & 0x80) P |= N;
+  else P &= ~N;
+
+  A = res;
+
+  return 1;
 }
