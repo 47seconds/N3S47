753421564ea27e42a160139c1011245fba0fbabb
47seconds <vaibhav@vocallabs.ai>
2026-03-26 00:19:15 +0530

    wrong understanding

--- header/cpu.h
+++ header/cpu.h
@@ -17,14 +17,14 @@ public:
   // P register flags
   // 00100100 at starting
   enum P_FLAGS {
-    C = 0,        // carry
-    Z = 0,        // zero
+    C = (1 << 0), // carry
+    Z = (1 << 1), // zero
     I = (1 << 2), // interrupt disable
-    D = 0,        // decimal
-    B = 0,        // B is 0 when pushed by interrupts (NMI and IRQ) and 1 when pushed by instructions (BRK and PHP)
+    D = (1 << 3), // decimal
+    B = (1 << 4), // B is 0 when pushed by interrupts (NMI and IRQ) and 1 when pushed by instructions (BRK and PHP)
     O = (1 << 5), // always pushed as one (no use, null)
-    V = 0,        // overflow
-    N = 0,        // negative
+    V = (1 << 6), // overflow
+    N = (1 << 7), // negative
   };
 
 private:
